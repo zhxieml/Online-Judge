16024ed21799064a006ea1f489a79657853c0b1a
fffffarmer <fffffarmer@gmail.com>
2018-10-23 22:05:31 +0800

    New

---
+++ module/linkQueue.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+template <class Type>
+class linkQueue
+{
+private:
+    struct node
+    {
+        Type data;
+
+        node *next;
+
+        node(const Type &x, node *n = NULL)
+        {
+            data = x;
+            next = n;
+        }
+
+        node(): next(NULL){}
+        ~node(){}
+    };
+
+    node *front, *rear;
+
+public:
+    linkQueue();
+    ~linkQueue();
+    bool isEmpty() const;
+    void enQueue(const Type &x);
+    Type deQueue();
+    Type getHead() const;
+};
+
+template <class Type>
+linkQueue<Type>::linkQueue()
+{
+    front = rear = NULL;
+}
+
+template <class Type>
+linkQueue<Type>::~linkQueue()
+{
+    node *tmp;
+    while (front != NULL)
+    {
+        tmp = front;
+        front = front->next;
+        delete tmp;
+    }
+}
+
+template <class Type>
+bool linkQueue<Type>::isEmpty() const
+{
+    return front == NULL;
+}
+
+template <class Type>
+Type linkQueue<Type>::getHead() const
+{
+    return front->data;
+}
+
+template <class Type>
+void linkQueue<Type>::enQueue(const Type &x)
+{
+    if (rear == NULL) front = rear = new node(x);
+    else rear = rear->next = new node(x);
+}
+
+template <class Type>
+Type linkQueue<Type>::deQueue()
+{
+    node *tmp = front;
+    Type value = front->data;
+    front = front->next;
+    if (front == NULL) rear = NULL; // empty
+    delete tmp;
+    return value;
+}
+
+
+
+
+
+
+
+
+
---
+++ module/seqQueue.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+template <class Type>
+class seqQueue
+{
+private:
+    Type *elem;
+    int maxSize;
+    int front, rear;
+    void doubleSpace();
+
+public:
+    seqQueue(int size = 100000);
+    ~seqQueue();
+    bool isEmpty() const;
+    void enQueue(const Type &x);
+    Type deQueue();
+    Type getHead() const;
+};
+
+template <class Type>
+seqQueue<Type>::seqQueue(int size)
+{
+    elem = new Type[size];
+    maxSize = size;
+    front = rear = 0;
+}
+
+template <class Type>
+seqQueue<Type>::~seqQueue()
+{
+    delete [] elem;
+}
+
+template <class Type>
+void seqQueue<Type>::doubleSpace()
+{
+    Type *tmp = elem;
+    elem = new Type[maxSize * 2];
+    for (int i = i; i < maxSize; i++) elem[i] = tmp[(front + i) % maxSize];
+    front = 0;
+    rear = maxSize - 1;
+    maxSize *= 2;
+    delete tmp;
+}
+
+template <class Type>
+bool seqQueue<Type>::isEmpty() const
+{
+    return front == rear;
+}
+
+template <class Type>
+void seqQueue<Type>::enQueue(const Type &x)
+{
+    if ((rear + 1) % maxSize == front) doubleSpace();
+    rear = (rear + 1) % maxSize;
+    elem[rear] = x;
+}
+
+template <class Type>
+Type seqQueue<Type>::deQueue()
+{
+    front = (front + 1) % maxSize;
+    return elem[front];
+}
+
+template <class Type>
+Type seqQueue<Type>::getHead() const
+{
+    return elem[(front + 1) % maxSize];
+}
