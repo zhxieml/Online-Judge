aa1db9737aa37042da3c318a8b5c2a7ec2af04eb
fffffarmer <fffffarmer@gmail.com>
2018-10-29 22:55:52 +0800

    New

---
+++ 1215.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+template<class Type>
+class priorityQueue
+{
+private:
+    int currentLength;
+    Type *elem;
+    int maxSize;
+
+    void doubleSpace();
+    void buildHeap();
+    void percolateDown(int hole);
+
+public:
+    priorityQueue(int capacity = 100);
+    priorityQueue(const Type data[], int size);
+    ~priorityQueue();
+    bool isEmpty() const;
+    void enQueue(const Type &x);
+    Type deQueue();
+    Type getHead() const;
+};
+
+template<class Type>
+priorityQueue<Type>::priorityQueue(int capacity)
+{
+    elem = new Type[capacity];
+    maxSize = capacity;
+    currentLength = 0;
+}
+
+template<class Type>
+priorityQueue<Type>::~priorityQueue()
+{
+    delete [] elem;
+}
+
+template<class Type>
+bool priorityQueue<Type>::isEmpty() const
+{
+    return currentLength == 0;
+}
+
+template<class Type>
+Type priorityQueue<Type>::getHead() const
+{
+    return elem[1];
+}
+
+template<class Type>
+void priorityQueue<Type>::doubleSpace()
+{
+    Type *tmp = elem;
+
+    maxSize *= 2;
+    elem = new Type[maxSize];
+    for (int i = 0; i <= currentLength; i++) elem[i] = tmp[i];
+    delete [] tmp;
+}
+
+template<class Type>
+void priorityQueue<Type>::enQueue(const Type &x)
+{
+    if (currentLength == maxSize - 1) doubleSpace();
+
+    int hole = ++currentLength;
+    for (; hole > 1 && x < elem[hole / 2]; hole /= 2) elem[hole] = elem[hole / 2];
+    elem[hole] = x;
+}
+
+template<class Type>
+Type priorityQueue<Type>::deQueue()
+{
+    Type minElem = elem[1];
+
+    elem[1] = elem[currentLength--];
+    percolateDown(1);
+    return minElem;
+}
+
+template<class Type>
+void priorityQueue<Type>::percolateDown(int hole)
+{
+    int child;
+    Type tmp = elem[hole];
+
+    for (; hole * 2 <= currentLength; hole = child)
+    {
+        child = hole * 2;
+
+        if (child != currentLength && elem[child + 1] < elem[child]) child++;
+
+        if (elem[child] < tmp) elem[hole] = elem[child];
+        else break;
+    }
+
+    elem[hole] = tmp;
+}
+
+template<class Type>
+void priorityQueue<Type>::buildHeap()
+{
+    for (int i = currentLength / 2; i > 0; i--) percolateDown(i);
+}
+
+template<class Type>
+priorityQueue<Type>::priorityQueue(const Type *items, int size): maxSize(size + 10), currentLength(size)
+{
+    elem = new Type[maxSize];
+    for (int i = 0; i < size; i++) elem[i + 1] = items[i];
+
+    buildHeap();
+}
+
+////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    char op[10];
+    int opNum;
+    priorityQueue<int> que;
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> op;
+
+        if (strcmp(op, "insert") == 0)
+        {
+            cin >> opNum;
+            que.enQueue(opNum);
+        }
+
+        if (strcmp(op, "delete") == 0)
+        {
+            que.deQueue();
+        }
+
+        if (strcmp(op, "min") == 0)
+        {
+            cout << que.getHead() << endl;
+        }
+    }
+}
