cc45e8914d9a6874c0d18fdd825dbe3e2f5a1941
fffffarmer <fffffarmer@gmail.com>
2018-11-01 21:53:08 +0800

    New

--- 1216.cpp
+++ 1216.cpp
@@ -4,88 +4,6 @@ using namespace std;
 
 const int INT_MAX = 2147483647;
 
-template <class Type>
-class linkQueue
-{
-private:
-    struct node
-    {
-        Type data;
-
-        node *next;
-
-        node(const Type &x, node *n = NULL)
-        {
-            data = x;
-            next = n;
-        }
-
-        node(): next(NULL){}
-        ~node(){}
-    };
-
-    node *front, *rear;
-
-public:
-    linkQueue();
-    ~linkQueue();
-    bool isEmpty() const;
-    void enQueue(const Type &x);
-    Type deQueue();
-    Type getHead() const;
-};
-
-template <class Type>
-linkQueue<Type>::linkQueue()
-{
-    front = rear = NULL;
-}
-
-template <class Type>
-linkQueue<Type>::~linkQueue()
-{
-    node *tmp;
-    while (front != NULL)
-    {
-        tmp = front;
-        front = front->next;
-        delete tmp;
-    }
-}
-
-template <class Type>
-bool linkQueue<Type>::isEmpty() const
-{
-    return front == NULL;
-}
-
-template <class Type>
-Type linkQueue<Type>::getHead() const
-{
-    return front->data;
-}
-
-template <class Type>
-void linkQueue<Type>::enQueue(const Type &x)
-{
-    if (rear == NULL) front = rear = new node(x);
-    else rear = rear->next = new node(x);
-}
-
-template <class Type>
-Type linkQueue<Type>::deQueue()
-{
-    node *tmp = front;
-    Type value = front->data;
-    front = front->next;
-    if (front == NULL) rear = NULL; // empty
-    delete tmp;
-    return value;
-}
-
-/////////////////////////////////////////////////////////////////
-
-
 template<class Type>
 class priorityQueue
 {
@@ -200,51 +118,10 @@ int priorityQueue<Type>::find(int x) const
     for (int i = 1; i <= currentLength; i++)
     {
         if (elem[i] > x && elem[i] < elem[minIndex]) minIndex = i;
-        // {
-        //     if (elem[i] < min)
-        //     {
-        //         min = elem[i];
-        //         minIndex = i;
-        //     }
-        // }
     }
 
     return minIndex;
 }
-
-// template<class Type>
-// int priorityQueue<Type>::find(int x) const
-// {
-//     int min = INT_MAX;
-//     int minIndex = 0;
-
-//     linkQueue<int> que;
-//     que.enQueue(1);
-//     int tmp;
-
-//     while (!que.isEmpty())
-//     {
-//         tmp = que.deQueue();
-
-//         if (elem[tmp] > x)
-//         {
-//             if (elem[tmp] == min) minIndex = (tmp < minIndex) ? tmp : minIndex;
-//             if (elem[tmp] < min)
-//             {
-//                 min = elem[tmp];
-//                 minIndex = tmp;
-//             }
-//         }
-
-//         else
-//         {
-//             if (tmp * 2 <= currentLength) que.enQueue(tmp * 2);
-//             if (tmp * 2 + 1 <= currentLength) que.enQueue(tmp * 2 + 1);
-//         }
-//     }
-
-//     return minIndex;
-// }
 
 template<class Type>
 void priorityQueue<Type>::decrease(int i, int v)
@@ -293,3 +170,4 @@ int main()
 
     return 0;
 }
+
