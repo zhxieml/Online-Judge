f4c90fbdcc2e589656882ee7596450c6cc2b6354
fffffarmer <fffffarmer@gmail.com>
2018-11-04 09:47:42 +0800

    New

---
+++ 1218.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{	
+    int n, opNum, tmp;
+    char op;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> op;
+
+        if (op == '+')
+        {
+            cin >> opNum;
+
+            for (int i = 0; i < opNum; i++)
+            
+        }
+    }
+
+    return 0;
+}
